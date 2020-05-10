d3d6807d518b2a5b5e9e6a39a28945f2813b24e3
Kilderson Sena <dersonsena@gmail.com>
2020-05-10 10:51:01 -0300

    implementation of the Behavior to be used in controllers

---
+++ src/JWTSignatureBehavior.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Dersonsena\JTWTools;
+
+use Dersonsena\JWTTools\JWTTools;
+use Yii;
+use yii\base\ActionFilter;
+use yii\web\UnauthorizedHttpException;
+
+class JWTSignatureBehavior extends ActionFilter
+{
+    /**
+     * @var string
+     */
+    public $headerName = 'Authorization';
+
+    /**
+     * @var string
+     */
+    public $secretKey;
+
+    /**
+     * @inheritDoc
+     */
+    public function beforeAction($action)
+    {
+        $authorizationHeader = Yii::$app->request->getHeaders()->get($this->headerName);
+
+        if (!$authorizationHeader) {
+            throw new UnauthorizedHttpException('Your request was made without an authorization token.');
+        }
+
+        $token = explode(' ', $authorizationHeader)[1];
+
+        $jwtTools = JWTTools::build($this->secretKey);
+
+        if ($jwtTools->tokenIsExpired($token)) {
+            throw new UnauthorizedHttpException('Authentication token is expired.');
+        }
+
+        if (!$jwtTools->signatureIsValid($token)) {
+            throw new UnauthorizedHttpException('The token signature is invalid.');
+        }
+
+        return true;
+    }
+}
